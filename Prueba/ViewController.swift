b7266ef4aa91edee88831789bf3467a664f21c1d
Alumno10 <alumno10@iMac-de-Administrador.local>
2021-02-18 19:28:19 +0100

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Alumno10 on 18/2/21.
+//  Copyright © 2021 Alumno10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
