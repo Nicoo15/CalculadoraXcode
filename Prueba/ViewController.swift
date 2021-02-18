decd299003303327f56723ebe285629091b51f82
Alumno10 <alumno10@iMac-de-Administrador.local>
2021-02-18 20:47:05 +0100

    primer commit

--- Prueba/ViewController.swift
+++ Prueba/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var resultado: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func sumar(_ sender: Any) {
+        let sumar : Int = Int(num1.text!)! + Int(num2.text!)!
+        resultado.text = String(sumar)
+        
+    }
+    
+    @IBAction func restar(_ sender: Any) {
+        let restar : Int = Int(num1.text!)! - Int(num2.text!)!
+        resultado.text = String(restar)
+    }
+    
+    @IBAction func multiplicar(_ sender: Any) {
+        let multiplicar : Int = Int(num1.text!)! * Int(num2.text!)!
+        resultado.text = String(multiplicar)
+    }
+    
+    @IBAction func dividir(_ sender: Any) {
+        let dividir : Int = Int(num1.text!)! / Int(num2.text!)!
+        resultado.text = String(dividir)
+        
+    }
+    
 }
 
